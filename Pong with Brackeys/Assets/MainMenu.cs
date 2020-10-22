a2cf92324332b97e5751ecb8e3643a6681b91072
Akshitbagri <akshitbagri2015@gmail.com>
2020-10-22 23:57:23 +0530

    Main Menu

---
+++ Pong with Brackeys/Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void QuitGame(){
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
+}
---
+++ Pong with Brackeys/Assets/OptionsMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour {
+	public AudioMixer audioMixer;
+
+	public void SetVolume (float volume){
+		audioMixer.SetFloat ("volume", volume);
+	}
+}
