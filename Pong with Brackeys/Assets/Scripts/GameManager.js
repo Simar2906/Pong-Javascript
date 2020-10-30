e8891486d24c290bd6822a11b5fe2b0563dd1d4c
Simar2906 <simar2906@gmail.com>
2020-10-30 16:39:01 +0530

    sidewalls converted to js

--- Pong with Brackeys/Assets/Scripts/GameManager.js
+++ Pong with Brackeys/Assets/Scripts/GameManager.js
@@ -1,24 +1,31 @@
 ﻿#pragma strict
 
-static var playerScore01 : int = 0;
-static var playerScore02 : int = 0;
+static var playerScore01: int = 0;
+static var playerScore02: int = 0;
+private var theBall: Transform = null;
+var theSkin: GUISkin;
 
-var theSkin : GUISkin;
-
-static function Score (wallName : String) {
-	if (wallName == "rightWall")
-	{
+private function Start() {
+	theBall = GameObject.FindGameObjectWithTag("Ball").transform;
+}
+static function Score(wallName: String) {
+	if (wallName == "rightWall") {
 		playerScore01 += 1;
 	}
-	else {
+	else if (wallName == "leftWall") {
 		playerScore02 += 1;
 	}
 	Debug.Log("Player Score 1 is " + playerScore01);
 	Debug.Log("Player Score 2 is " + playerScore02);
 }
 
-function OnGUI () {
+function OnGUI() {
 	GUI.skin = theSkin;
-	GUI.Label (new Rect (Screen.width/2-150, 25, 100, 100), "" + playerScore01);
-	GUI.Label (new Rect (Screen.width/2+150, 25, 100, 100), "" + playerScore02);
+	GUI.Label(new Rect(Screen.width/2 - 150 -18, 25, 100, 100), "" + playerScore01);
+	GUI.Label(new Rect(Screen.width/2 + 150 -18, 25, 100, 100), "" + playerScore02);
+	if (GUI.Button(new Rect(Screen.width/2 -150/2, 35, 121, 53), "RESET")) {
+		playerScore01 = 0;
+		playerScore02 = 0;
+		theBall.SendMessage("ResetBall");
+	}
 }
---
+++ Pong with Brackeys/Assets/Scripts/SideWalls.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var score : AudioSource = null;
+function OnTriggerEnter2D (hitInfo : Collider2D) {
+	if (hitInfo.name == "Ball")
+	{
+		var wallName = transform.name;
+		GameManager.Score (wallName);
+		hitInfo.gameObject.SendMessage("ResetBall");
+		score.Play();
+	}
+}
