bbc228485436adec3445d8ba965828cc22d7be2c
Akshitbagri <akshitbagri2015@gmail.com>
2020-11-12 19:34:00 +0530

    levels added

--- Pong with Brackeys/Assets/Scripts/Menu/MainMenu.js
+++ Pong with Brackeys/Assets/Scripts/Menu/MainMenu.js
@@ -10,6 +10,9 @@ public function PlaySinglePlayer(){
 	SceneManager.LoadScene(2);
 }
 
+public function PlaySinglehardPlayer(){
+	SceneManager.LoadScene(3);
+}
 public function QuitGame() {
     Debug.Log("Quit");
     Application.Quit();
